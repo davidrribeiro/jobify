cbcedb2d31c22d19ec521123ee911e4910b3e15b
talkdsign <talkdsign@gmail.com>
2020-01-03 20:51:31 -0300

    Remove bloco hostname

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const port = process.env.PORT || 3000
 
-app.use('/admin', (req, res, next) => {
-    if(req.hostname === 'localhost') {
-        next()
-    }else{
-        res.send('Área restrita')
-    }
+//Acessa o admin somente do localhost
+
+// app.use('/admin', (req, res, next) => {
+//     if(req.hostname === 'localhost') {
+//         next()
+//     }else{
+//         res.send('Área restrita')
+//     }
     
-})
+// })
 
 app.set('views', path.join(__dirname, 'views'))
 
