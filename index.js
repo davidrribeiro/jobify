1470ee6f90d97e57513e28a42d0c465d5e8be0c0
talkdsign <talkdsign@gmail.com>
2020-01-03 18:15:53 -0300

    edita registros, busca categorias no banco

--- index.js
+++ index.js
@@ -49,13 +49,30 @@ app.get('/admin/vagas/delete/:id', async(req, res) =>{
 })
 
 app.get('/admin/vagas/nova', async(req, res) =>{
-    res.render('admin/nova-vaga')
+    const db = await dbConnection
+    const categorias = await db.all('select * from categorias')
+    res.render('admin/nova-vaga', { categorias })
 })
 
 app.post('/admin/vagas/nova', async(req, res) =>{
     const { titulo, descricao, categoria } = req.body
     const db = await dbConnection
     await db.run(`insert into vagas(categoria, titulo, descricao) values(${categoria}, '${titulo}', '${descricao}')`)
+    res.redirect('/admin/vagas')
+})
+app.get('/admin/vagas/editar/:id', async(req, res) =>{
+    const db = await dbConnection
+    const categorias = await db.all('select * from categorias')
+    const vaga = await db.get('select * from vagas where id ='+req.params.id)
+    res.render('admin/editar-vaga', { categorias, vaga })
+
+})
+
+app.post('/admin/vagas/editar/:id', async(req, res) =>{
+    const { titulo, descricao, categoria } = req.body
+    const { id } = req.params
+    const db = await dbConnection
+    await db.run(`update vagas set categoria = ${categoria} , titulo ='${titulo}' , descricao ='${descricao}' where id = ${id}`)
     res.redirect('/admin/vagas')
 })
 
