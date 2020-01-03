ccc0511e377bc31aeefefe50d94a42e07f7817ce
talkdsign <talkdsign@gmail.com>
2020-01-03 20:48:30 -0300

    restringe acesso a localhost

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const port = process.env.PORT || 3000
 
+app.use('/admin', (req, res, next) => {
+    if(req.hostname === 'localhost') {
+        next()
+    }else{
+        res.send('Área restrita')
+    }
+    
+})
+
 app.set('views', path.join(__dirname, 'views'))
 
 app.set('view engine', 'ejs')
